1b2b56a58b4be1ba3cd4a89eb2ee6080376aacb8
Nicolas Mailloux <nicolecrivain@gmail.com>
2022-08-24 13:01:19 -0400

    Progress

--- init.c
+++ init.c
@@ -57,25 +57,130 @@ int main() {
 	// Kernel version
 	struct utsname uname_data;
 	uname(&uname_data);
-	kernel_version = uname_data.version;
+	kernel_version = uname_data.sysname;
+	strncat(kernel_version, space, sizeof(space));
+	strncat(kernel_version, uname_data.nodename, sizeof(uname_data.nodename));
+	strncat(kernel_version, space, sizeof(space));
+	strncat(kernel_version, uname_data.version, sizeof(uname_data.version));
 	// Kernel build ID
 	kernel_build_id = read_file("/opt/build_id");
+	kernel_build_id[strcspn(kernel_build_id, "\n")] = 0;
 	// Kernel Git commit
 	kernel_git_commit = read_file("/opt/commit");
+	kernel_git_commit[strcspn(kernel_git_commit, "\n")] = 0;
+	// Setting up boot flags partition (P1)
+	mount("/dev/mmcblk0p1", "/mnt", "ext4", 0, "");
+	mkdir("/mnt/flags", 0755);
+	umount("/mnt");
+
+	// Handling DISPLAY_DEBUG flag (https://inkbox.ddns.net/wiki/index.php?title=Boot_flags)
+	display_debug = read_file("/mnt/flags/DISPLAY_DEBUG");
+	if(strcmp(display_debug, "true\n") == 0 || strcmp(display_debug, "true") == 0) {
+		mkfifo(serial_fifo_path, 0x29A);
+		const char * arguments[] = { "/etc/init.d/inkbox-splash", "display_debug", NULL }; run_command("/etc/init.d/inkbox-splash", arguments, false);
+		sleep(5);
+		// Redirecting all stdout output to display debug's named pipe
+		freopen(serial_fifo_path, "a+", stdout);
+	}
+
+	// Information header
+	printf("\n%s GNU/Linux\nInkBox OS, kernel build %s, commit %s\n\n", kernel_version, kernel_build_id, kernel_git_commit);
+	
+	// Checking filesystems
+	info("Checking filesystems ...", 0);
+	printf("\n");
+	{
+		// P1
+		const char * arguments[] = { "/usr/bin/fsck.ext4", "-y", "/dev/mmcblk0p1", NULL };
+		run_command("/usr/bin/fsck.ext4", arguments, true);
+	}
+	{
+		// P2
+		const char * arguments[] = { };
+		arguments[0] = "/usr/bin/fsck.ext4", arguments[1] = "-y";
+		if(strcmp(device, "n873") == 0) {
+			arguments[2] = "/dev/mmcblk0p5";
+		}
+		else {
+			arguments[2] = "/dev/mmcblk0p2";
+		}
+		arguments[3] = NULL;
+		run_command("/usr/bin/fsck.ext4", arguments, true);
+	}
+	{
+		// P3
+		const char * arguments[] = { "/usr/bin/fsck.ext4", "-y", "/dev/mmcblk0p3", NULL };
+		run_command("/usr/bin/fsck.ext4", arguments, true);
+	}
+	{
+		// P4
+		const char * arguments[] = { "/usr/bin/fsck.ext4", "-y", "/dev/mmcblk0p4", NULL };
+		run_command("/usr/bin/fsck.ext4", arguments, true);
+	}
+	printf("\n");
+
+	// Universal ID check
+	{
+		const char * arguments[] = { "/opt/bin/uidgen", NULL }; run_command("/opt/bin/uidgen", arguments, true);
+	}
+
+	// DONT_BOOT
+	dont_boot = read_file("/mnt/flags/DONT_BOOT");
+	if(strcmp(dont_boot, "true\n") == 0 || strcmp(dont_boot, "true") == 0) {
+		info("Device is locked down and will not boot.", 2);
+	}
+
+	// ENCRYPT_LOCK
+	encrypt_lock = read_file("/mnt/flags/ENCRYPT_LOCK");
+	if(encrypt_lock[0] != '\0') {
+		unsigned long current_epoch = time(NULL);
+		unsigned long lock_epoch = strtoul(encrypt_lock, NULL, 0);
+		// Comparing lockdown time limit to current time
+		if(current_epoch < lock_epoch) {
+			info("Too many incorrect encrypted storage unlocking attempts have locked down this device. Shutting down ...", 2);
+			const char * arguments[] = { "/etc/init.d/inkbox-splash", "alert_splash", "7", NULL }; run_command("/etc/init.d/inkbox-splash", arguments, true);
+			sync();
+			exit(EXIT_FAILURE);
+			// rcS will power off the device after this
+		}
+		else {
+			// Avoid running this block of code again
+			remove("/mnt/flags/ENCRYPT_LOCK");
+		}
+	}
+
+	// DFL
+	dfl = read_file("/mnt/flags/DFL");
+	if(strcmp(dfl, "true\n") == 0 || strcmp(dfl, "true") == 0) {
+		info("Entering Direct Firmware Loader mode (DFL) ...", 0);
+		// Re-setting flag
+		write_file("/mnt/flags/DFL", "false\n");
+		launch_dfl();
+	}
+
+	// BOOT_USB_DEBUG
+	boot_usb_debug = read_file("/mnt/flags/BOOT_USB_DEBUG");
+	if(strcmp(boot_usb_debug, "true\n") == 0 || strcmp(boot_usb_debug, "true") == 0) {
+		setup_usb_debug(true);
+	}
 }
 
 // https://github.com/Kobo-InkBox/inkbox-power-daemon/blob/8296c4a1811e3921ff98e9980504c24d23435dac/src/functions.cpp#L415-L430
-void run_command(const char * path, const char * args[], bool wait) {
+bool run_command(const char * path, const char * arguments[], bool wait) {
 	int status = -1;
 	int pid = 0;
 
-	status = posix_spawn(&pid, path, NULL, NULL, (char**)args, NULL);
+	status = posix_spawn(&pid, path, NULL, NULL, (char**)arguments, NULL);
 	if(status != 0) {
 		fprintf(stderr, "Error in spawning process %s\n", path);
+		return false;
 	}
+	
 	if(wait == true) {
 		waitpid(pid, 0, 0);
 	}
+
+	return true;
 }
 
 // https://stackoverflow.com/a/230070/14164574
@@ -86,30 +191,48 @@ bool file_exists(char * file_path) {
 
 // https://stackoverflow.com/a/3747128/14164574
 char * read_file(char * file_path) {
-	FILE * fp;
-	long lSize;
-	char * buffer;
+	// Ensure that specified file exists, then try to read it
+	if(access(file_path, F_OK) == 0) {
+		FILE * fp;
+		long lSize;
+		char * buffer;
 
-	fp = fopen(file_path , "rb");
-	if(!fp) perror("Error opening file"), exit(1);
+		fp = fopen(file_path , "rb");
 
-	fseek(fp, 0L , SEEK_END);
-	lSize = ftell(fp);
-	rewind(fp);
+		fseek(fp, 0L , SEEK_END);
+		lSize = ftell(fp);
+		rewind(fp);
 
-	/* Allocate memory for entire content */
-	buffer = calloc(1, lSize+1);
-	if(!buffer) fclose(fp), fputs("Memory allocation failed", stderr), exit(1);
+		/* Allocate memory for entire content */
+		buffer = calloc(1, lSize+1);
+		if(!buffer) fclose(fp);
 
-	/* Copy the file into the buffer */
-	if(1 != fread(buffer, lSize, 1, fp)) {
-		fclose(fp),free(buffer),fputs("File reading failed", stderr), exit(1);
+		/* Copy the file into the buffer */
+		if(1 != fread(buffer, lSize, 1, fp)) {
+			fclose(fp);
+		}
+
+		return buffer;
+
+		fclose(fp);
+		free(buffer);
 	}
+	else {
+		return "";
+	}
+}
 
-	return buffer;
+// https://stackoverflow.com/a/14576624/14164574
+bool write_file(char * file_path, char * content) {
+	FILE *file = fopen(file_path, "w");
 
-	fclose(fp);
-	free(buffer);
+	int rc = fputs(content, file);
+	if (rc == EOF) {
+		return false;
+	}
+	else {
+		fclose(file);
+	}
 }
 
 // https://github.com/Kobo-InkBox/inkbox-power-daemon/blob/8296c4a1811e3921ff98e9980504c24d23435dac/src/wifi.cpp#L181-L197
@@ -160,4 +283,75 @@ int set_if_flags(char * ifname, short flags) {
 
 int set_if_up(char * ifname) {
     return set_if_flags(ifname, IFF_UP);
+}
+
+int info(char * message, int mode) {
+	/*
+	 * Modes:
+	 * - 0: Normal logging (green)
+	 * - 1: Warning logging (yellow)
+	 * - 2: Error logging (red)
+	*/
+	if(mode == 0) {
+		printf("\e[1m\e[32m * \e[0m%s\n", message);
+	}
+	else if(mode == 1) {
+		printf("\e[1m\e[33m * \e[0m%s\n", message);
+	}
+	else if(mode == 2) {
+		printf("\e[1m\e[31m * \e[0m%s\n", message);
+	}
+	else {
+		return 1;
+	}
+	return 0;
+}
+
+void launch_dfl() {
+	// Loading USB Mass Storage (USBMS) modules
+	mkdir("/modules", 0755);
+	mount("/opt/modules.sqsh", "/modules", "squashfs", 0, "");
+	if(strcmp(device, "n705") == 0 || strcmp(device, "n905b") == 0 || strcmp(device, "n905c") == 0 || strcmp(device, "n613") == 0) {
+		load_module("/modules/arcotg_udc.ko", "");
+	}
+	if(strcmp(device, "n306") == 0 || strcmp(device, "n873") == 0) {
+		load_module("/modules/fs/configfs/configfs.ko", "");
+		load_module("/modules/drivers/usb/gadget/libcomposite.ko", "");
+		load_module("/modules/drivers/usb/gadget/function/usb_f_mass_storage.ko", "");
+	}
+	if(strcmp(device, "emu") != 0) {
+		load_module("/modules/g_mass_storage.ko", "");
+	}
+
+	// Splash time
+	const char * arguments[] = { "/etc/init.d/inkbox-splash", "dfl", NULL }; run_command("/etc/init.d/inkbox-splash", arguments, true);
+	while(true) {
+		info("This device is in DFL mode. Please reset it to resume normal operation.", 0);
+		sleep(30);
+	}
+}
+
+void setup_usb_debug(bool boot) {
+	mkdir("/dev/pts", 0755);
+	mount("devpts", "/dev/pts", "devpts", 0, "");
+	{
+		// Telnet server
+		const char * arguments[] = { "/bin/busybox", "telnetd", NULL }; run_command("/bin/busybox", arguments, false);
+	}
+	{
+		// FTP server
+		const char * arguments[] = { "/bin/busybox", "tcpsvd", "-vE", "0.0.0.0", "21", "ftpd", "-A", "-w", "/", NULL }; run_command("/bin/busybox", arguments, false);
+	}
+
+	if(boot == true) {
+		setup_usbnet();
+		const char * arguments[] = { "/etc/init.d/inkbox-splash", "usb_debug", NULL }; run_command("/etc/init.d/inkbox-splash", arguments, true);
+		while(true) {
+			info("This device is in boot-time USB debug mode. Please reset or reboot it to resume normal operation.", 0);
+			sleep(30);
+		}
+	}
+}
+
+void setup_usbnet() {
 }
--- init.h
+++ init.h
@@ -17,23 +17,36 @@
 #include <sys/utsname.h>
 #include <linux/module.h>
 #include <net/if.h>
+#include <time.h>
 
 // Defines
 #define initModule(module_image, len, param_values) syscall(__NR_init_module, module_image, len, param_values)
 
 // Variables
 char * device;
+char * space = " ";
 static int skfd = -1; // AF_INET socket for ioctl() calls
+char * serial_fifo_path = "/tmp/serial-fifo";
 char * kernel_version;
 char * kernel_build_id;
 char * kernel_git_commit;
+char * display_debug;
+char * dont_boot;
+char * encrypt_lock;
+char * dfl;
+char * boot_usb_debug;
 
 // Functions
-void run_command(const char * path, const char * args[], bool wait);
+bool run_command(const char * path, const char * arguments[], bool wait);
 bool file_exists(char * file_path);
 char * read_file(char * file_path);
+bool write_file(char * file_path, char * content);
 int load_module(char * module_path, char * params);
 int set_if_flags(char * ifname, short flags);
 int set_if_up(char * ifname);
+int info(char * message, int mode);
+void launch_dfl();
+void setup_usb_debug(bool boot);
+void setup_usbnet();
 
 #endif // INIT_H
